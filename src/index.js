1d112ce46d8d75fcb43e54080a2cd9be833c84bb
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-02-12 14:12:48 +0100

    feat(epic): create application

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import FrameSDK from "@farcaster/frame-sdk";
+
+document.addEventListener("DOMContentLoaded", async function() {
+    setTimeout(() => {
+        FrameSDK.actions.ready();
+        console.log("Farcaster Frame is ready!");
+    }, 500);
+
+    try {
+        const ipfsHash = "Qmcw111vvxnrwXjFft5dpj1dXJD4Lr1TX26qPLFXt9ggTY";
+        const response = await fetch("https://ipfs.io/ipfs/"+ipfsHash);
+        const data = await response.json();
+
+        document.getElementById('main-image-caption').innerHTML = data.images_collected_at;
+        const mainImageUrl = 'https://ipfs.io/ipfs/'+data.asset_ipfs_hash;
+        const mainImage = document.getElementById("main-image");
+        mainImage.src = mainImageUrl
+
+        const thumbnailsContainer = document.getElementById("thumbnails");
+
+        const img = document.createElement("img");
+        img.src = mainImageUrl
+        img.className = "thumbnail";
+        img.onclick = () => {
+            mainImage.src = mainImageUrl
+        };
+        thumbnailsContainer.appendChild(img);
+
+        data.images.forEach(image => {
+            const img = document.createElement("img");
+            img.src = image.thumbnail;
+            img.className = "thumbnail";
+            img.onclick = () => {
+                //mainImage.src = url.replace('thumbs', 'png').replace('\.jpg', '\.png');
+                mainImage.src = image.url;
+            };
+            thumbnailsContainer.appendChild(img);
+        });
+
+    } catch (error) {
+        console.error("Erreur lors du chargement du JSON:", error);
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+        patterns: [
+          { from: "src/.well-known", to: ".well-known" },
+          { from: "src/data.json", to: "data.json" },
+          { from: "src/styles.css", to: "styles.css" },
+        ]
+      }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+};
